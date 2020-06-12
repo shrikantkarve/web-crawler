b457d16212eebdda8c3f0f1ae970000feff6303a
Karve <Shrikant.Karve@emc.com>
2020-06-12 23:03:17 +0530

    Basic skeleton

---
+++ crawler.py
@@ -0,0 +1,14 @@
+import requests
+import re
+
+class PyCrawler(object):
+    def init(self, starting_url):
+        self.starting_url = starting_url
+        self.visited = set()
+
+    def start(self):
+        pass
+
+if __name__ == "__main__":
+    crawler = PyCrawler()
+    crawler.start()
