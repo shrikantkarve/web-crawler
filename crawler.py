7ef229b3ae8de84d24596cfdab5d1321f5f5679e
skarve <skarve@isilon.com>
2021-03-29 16:52:15 +0530

    Working crawler code

--- crawler.py
+++ crawler.py
@@ -1,32 +1,62 @@
 from bs4 import BeautifulSoup
 import requests
+from urllib.parse import urlparse
 
-html_doc1 = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+url = "http://www.yahoo.com"
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+class DomainCrawler():
+    def __init__(self, starting_url):
+        self.starting_url = starting_url
+        self.parsed_starting_url = urlparse(starting_url)
+        #print(self.starting_url)
+        self.visited_urls = []
+        self.processed_pages = 0
+        self.max_process_pages = 100
 
-<p class="story">...</p>
-"""
+    def start(self):
+        self.process_page(self.starting_url)
 
-url = "https://webscraper.io/test-sites/tables"
+    def process_page(self, href):
+        if self.processed_pages >= self.max_process_pages:
+            return
+        
+        url = urlparse(href)
+        if url.netloc == "":
+            href = self.parsed_starting_url.scheme + '://' +\
+                   self.parsed_starting_url.netloc +\
+                   href
+        
+        if href not in self.visited_urls:
+            self.visited_urls.append(href)
+        else:
+            return
+
+        try:
+            response = requests.get(href)
+        except:
+            return
+        
+        print('[*] Processing %s' % href)
+        self.processed_pages += 1
+
+        soup = BeautifulSoup(response.text,'html.parser')
+        
+        for link in soup.find_all('a'):
+            # process the link to check if it is internal
+            if self.is_the_url_internal(link.get('href')):
+                self.process_page(link.get('href'))
+        
+
+    def is_the_url_internal(self, href):
+        url = urlparse(href)
+        if url.netloc == "" or url.netloc == self.parsed_starting_url.netloc:
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
-    r = requests.get(url)
-    data = r.text
-    soup = BeautifulSoup(data,'html.parser')
-    #print(soup.prettify)
-    l = []
-    for link in soup.find_all('a'):
-        l.append(link.get('href'))
-    
-    for _ in l:
-        print(_)
+    crawler = DomainCrawler(url)
+    crawler.start()
 
+    
 
